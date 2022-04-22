c1e0e6d7775fa37ac273e5f02fdd1c7c43471d2d
Alessio240593 <alessio.zattoni93@gmail.com>
2022-04-22 16:09:54 +0200

    2022/04/22

---
+++ src/server.c
@@ -0,0 +1,12 @@
+/// @file sender_manager.c
+/// @brief Contiene l'implementazione del sender_manager.
+
+#include "err_exit.h"
+#include "defines.h"
+#include "shared_memory.h"
+#include "semaphore.h"
+#include "fifo.h"
+
+int main(int argc, char * argv[]) {
+    return 0;
+}
