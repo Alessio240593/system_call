10a5f2b2f6d5912445bdebae62bc2d926d39eec4
Alessio240593 <alessio.zattoni93@gmail.com>
2022-04-24 12:00:10 +0200

    sistemato le sigmask

--- src/client_0.c
+++ src/client_0.c
@@ -38,29 +38,29 @@ void sigint_handler(int sig)
 
 int main(int argc, char * argv[]) {
     if(argc < 2){
-        printf("Use ./Client_0 <path>");
+        printf("Use ./Client_0 <path>\n");
         exit(EXIT_FAILURE);
     }
     else if(argc > 2){
-        printf("Too many arguments");
+        printf("Too many arguments\n");
         exit(EXIT_FAILURE);
     }
 
     sigset_t mySet;
 
-    if((sigemptyset(&mySet))== -1){
+    if((sigfillset(&mySet))== -1){
         errExit("sigemptyset");
     }
 
-    if((sigaddset(&mySet, SIGINT)) || (sigaddset(&mySet, SIGUSR1)) == -1){
+    if((sigdelset(&mySet, SIGINT)) || (sigdelset(&mySet, SIGUSR1)) == -1){
         errExit("sigaddset");
     }
 
-    if((sigprocmask(SIG_UNBLOCK, &mySet, NULL)) == -1){
+    if((sigprocmask(SIG_SETMASK, &mySet, NULL)) == -1){
         errExit("sigprocmask");
     }
 
-    if((signal(SIGINT, sigint_handler)) || (signal(SIGUSR1, sigusr1_handler)) == SIG_ERR){
+    if(((signal(SIGINT, sigint_handler)) || (signal(SIGUSR1, sigusr1_handler))) == SIG_ERR){
         errExit("signal");
     }
     pause(); //wait for a signal
