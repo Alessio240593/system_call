7a347cf56879029b3d4efe642fbfdcc073c50150
Alessio240593 <alessio.zattoni93@gmail.com>
2022-05-09 21:55:02 +0200

    aggiunto script per velocizzare testing

---
+++ clean.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+DIR="/tmp/myDir"
+FIFO1="/tmp/fifo1"
+FIFO2="/tmp/fifo2"
+
+if [[ ! -d $DIR ]];
+then
+	mkdir -pv /tmp/myDir/{a,b,c,d,e,}/{aa,bb,cc,dd,ee,}
+	for dir in $(find /tmp/myDir -type d) ; do echo "$(date +%s)" > "$dir"/sendme_"$(date +%N)" ; done
+	echo "$(date +%s)" > /tmp/myDir/sendme_"$(date +%N)"
+fi
+
+if [ -p $FIFO1 ] || [ -p $FIFO2 ];
+then
+	rm $FIFO1 && rm $FIFO2
+	echo "fifo removed succesfully!"
+fi
+
+ipcrm -a
+echo "ipcs removed succesfully!"
+
